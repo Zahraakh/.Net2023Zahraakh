02274e4fd24b96e4348639cbf1715bc7525b6324
Toshiba <Toshiba@DESKTOP-KJHM2HF>
2023-08-10 15:08:25 +0300

    db

--- WebApplication2/Models/Product.cs
+++ WebApplication2/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CmsShoppingCart.Models
@@ -16,6 +17,7 @@ namespace CmsShoppingCart.Models
         [Column(TypeName ="decimal(18,2)")]
         public decimal Price { get; set; }
 
+        [Display(Name ="Category")]
         public int CategoryId { get; set; }
 
         public string Image { get; set; }
@@ -25,5 +27,8 @@ namespace CmsShoppingCart.Models
 
         [ForeignKey("CategoryId")]
         public virtual Category Category { get; set; }
+
+        [NotMapped]
+        public IFormFile ImageUpload { get; set; }
     }
 }
