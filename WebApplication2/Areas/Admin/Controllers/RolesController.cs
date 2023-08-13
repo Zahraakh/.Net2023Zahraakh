16362479b4a68d93a2be9d85f3498152feb8b922
Toshiba <Toshiba@DESKTOP-KJHM2HF>
2023-08-14 01:10:10 +0300

    db

---
+++ WebApplication2/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,20 @@
+﻿using CmsShoppingCart.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<AppUser> userManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
+        {
+            this.roleManager = roleManager;
+            this.userManager = userManager;
+        }
+        public IActionResult Index() => View(roleManager.Roles);
+        
+    }
+}
