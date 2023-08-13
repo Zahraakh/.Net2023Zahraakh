88a39c40c2c3db96b2f91720a8be833178dca051
Toshiba <Toshiba@DESKTOP-KJHM2HF>
2023-08-13 03:48:14 +0300

    db

---
+++ WebApplication2/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CmsShoppingCart.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        public string Index()
+        {
+            return "hello";
+        }
+    }
+}
