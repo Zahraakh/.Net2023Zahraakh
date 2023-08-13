cc6bfe259f42cd6f844ff255f22a5df60988ffc2
Toshiba <Toshiba@DESKTOP-KJHM2HF>
2023-08-13 19:55:26 +0300

    db

---
+++ WebApplication2/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using CmsShoppingCart.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CmsShoppingCart.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+
+        private readonly UserManager<AppUser> userManager;
+
+        public UserController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager,
+            IPasswordHasher<AppUser> passwordHasher)
+        {
+            this.userManager = userManager;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+    }
+}
