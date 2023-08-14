254f2212057b9fa98885437fcc34da5b3b0ce8e0
Toshiba <Toshiba@DESKTOP-KJHM2HF>
2023-08-14 03:41:23 +0300

    db

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -34,6 +34,8 @@ namespace CmsShoppingCart
                 //options.IdleTimeout = TimeSpan.FromDayss(2)
             });
 
+            services.AddRouting(options => options.LowercaseUrls = true);
+
             services.AddControllersWithViews();
 
             services.AddDbContext<CmsShoppingCartContext>(options => options.UseSqlServer
